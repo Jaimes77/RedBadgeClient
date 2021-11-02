b8c7965b2490c7c515e5c30644fbc25bd631e563
Jaimes77 <JaimeRGoff@gmail.com>
2021-11-02 18:38:41 -0400

    register and login working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Journal from "./pages/Journal";
+import Calendar from "./pages/Calendar";
+import Meds from "./pages/Meds";
 
-function App() {
-  return <div className="App">Hello from Caregivers R Us</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Register} />
+            <Route exact path={"/login"} component={Login} />
+            <Route exact path={"/journal"} component={Journal} />
+            <Route exact path={"/meds"} component={Meds} />
+            <Route exact path={"/calendar"} component={Calendar} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
